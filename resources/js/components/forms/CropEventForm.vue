bd32bda94e32cb815e8cb3ceb893a7c3b798cf1f
Matt Finnigan <mfwebdesign01@gmail.com>
2024-12-04 10:16:28 +1100

    add new action option

--- resources/js/components/forms/CropEventForm.vue
+++ resources/js/components/forms/CropEventForm.vue
@@ -7,8 +7,8 @@
         <Display v-else label="Plant" :val="currPlant.name + ' (' + currPlant.variety + ')'"/>
         <Select v-model.number="currEvent.location_id" label="Select a Location" :options="locations.map(l => { return { label: l.name, value: l.id } })"/>
         <Select v-if="currLocation" v-model.number="currEvent.bed_id" label="Select a Bed (Optional)" :options="currLocation.beds.map(b => { return { label: b.name, value: b.id } })"/>
-        <Select v-model="currEvent.action" label="Action" :options="['Planned', 'Sowed', 'Transplanted', 'Moved', 'Fertilized', 'Watered', 'Weeded', 'Pesticided', 'Pruned', 'Harvested'].map(a => { return { label: a, value: a } })"/>
-        <Select v-model="currEvent.stage" label="Lifecycle Stage" :options="['Planned', 'Germination', 'Seedling', 'Vegetative', 'Flowering', 'Fruiting', 'Complete'].map(s => { return { label: s, value: s } })"/>
+        <Select v-model="currEvent.action" label="Action" :options="actionOptions"/>
+        <Select v-model="currEvent.stage" label="Lifecycle Stage" :options="stageOptions"/>
         <Input v-model="currEvent.datetimestamp" type="datetime-local" label="Date & Time"/>
         <Input v-model="currEvent.qty" type="number" label="Quantity" required/>
         <Input v-model="currEvent.notes" type="textarea" label="Notes"/>
@@ -71,6 +71,12 @@ export default {
     currLocation () {
       return this.locations.find(l => l.id === this.currEvent?.location_id)
     },
+    actionOptions () {
+      return ['Planned', 'Sowed', 'Transplanted', 'Moved', 'Fertilized', 'Watered', 'Weeded', 'Damage/Disease detected', 'Sprayed', 'Pruned', 'Harvested'].map(a => { return { label: a, value: a } })
+    },
+    stageOptions () {
+      return ['Planned', 'Germination', 'Seedling', 'Vegetative', 'Flowering', 'Fruiting', 'Complete'].map(s => { return { label: s, value: s } })
+    }
   },
   methods: {
     submitForm (e) {
